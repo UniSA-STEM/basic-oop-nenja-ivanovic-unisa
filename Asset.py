938792ed6da52851e1119475afe8552e889e29f7
Nenja Ivanovic <ivany005@mymail.unisa.edu.au>
2025-10-16 23:33:34 +1100

    Create Asset constructor and attribute getters/setters
    - Add getters for name, description and encrypted attributes.
    - Add encrypted attribute setter via decrypt() and encrypt() methods
    -Implement Asset class constructor with name and description as instance attributes.

--- Asset.py
+++ Asset.py
@@ -6,3 +6,25 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+
+class Asset:
+    def __init__(self, name: str, description: str):
+        self.__name = name
+        self.__description = description
+        self.__encrypted = False
+
+    def get_name(self):
+        return self.__name
+
+    def get_description(self):
+        return self.__description
+
+    def get_encrypted(self):
+        return self.__encrypted
+
+    def encrypt(self) -> None:
+        self.__encrypted = True
+
+    def decrypt(self) -> None:
+        self.__encrypted = False
