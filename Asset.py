95ee45c1d0965356344b385f0a9651ab64f5071a
Nenja Ivanovic <ivany005@mymail.unisa.edu.au>
2025-10-18 22:18:08 +1100

    Update Asset.py constructor
    Create a class attribute of Asset.py ('available_assets') which is a list containing the valid items able to be created.
    
    Instead of providing the description each time an Asset is created, this is done automatically in the constructor based on the provided name.

--- Asset.py
+++ Asset.py
@@ -9,10 +9,23 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 
 class Asset:
-    def __init__(self, name: str, description: str):
+    available_assets = ["CryptoToken", "Dataspike", "Removable_Drive", "SecurityChip", "HardwarePatch"]
+
+    def __init__(self, name: str):
+        assert name in Asset.available_assets, f"The item '{name}' does not exist. Available assets are: {Asset.available_assets}"
         self.__name = name
-        self.__description = description
         self.__encrypted = False
+
+        if name == "CryptoToken:":
+            self.__description = "Used to acquire or repair rigs."
+        elif name == "Dataspike":
+            self.__description = "Used in battles."
+        elif name == "Removable_Drive":
+            self.__description = "Found in rigs and used for extraction."
+        elif name == "SecurityChip":
+            self.__description = "Used to encrypt or decrypt assets."
+        else:
+            self.__description = "Used to upgrade rigs."
 
     def get_name(self):
         return self.__name
